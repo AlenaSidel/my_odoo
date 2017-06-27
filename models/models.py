741c757b428086eb3bb311207eebdeb66a110ab3
Alena Sidel <sidelnikova.alean@gmail.com>
2017-06-27 16:51:19 +0500

    Добавила пустые строки. Убрала _name. Удалила demo. Убрала зависимость от base (не обратила на нее внимание сразу, она автоматически была при создании модуля)

--- models/models.py
+++ models/models.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-import logging
-
-_logger = logging.getLogger(__name__)
 
 class LeadLogg(models.Model):
-    _name = "crm.lead"
     _inherit = "crm.lead"
 
     @api.multi
     def write(self, vals):
-        #_logger.info("mail change!")
         if vals.get('email_from'):
             self.message_post("Mail change! New mail: %s" % vals.get('email_from'))
 
