7b06db1ff3c558491d446f44ed29c2b6dd229eeb
Alena Sidel <sidelnikova.alean@gmail.com>
2017-06-27 22:36:50 +0500

    Переопределение поля email_from

--- models/models.py
+++ models/models.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields
+
 
 class LeadLogg(models.Model):
     _inherit = "crm.lead"
 
-    @api.multi
-    def write(self, vals):
-        if vals.get('email_from'):
-            self.message_post("Mail change! New mail: %s" % vals.get('email_from'))
-
-        return super(LeadLogg, self).write(vals)
+    email_from = fields.Char('Email', help="Email address of the contact",
+                             index=True, track_visibility='onchange')
