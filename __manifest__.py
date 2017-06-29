f7088cfb6c1bbb888ead89bf9dc427c4c35e97df
Alena Sidel <sidelnikova.alean@gmail.com>
2017-06-29 11:10:15 +0500

    Убираем лишнее (controllers, views)

--- __manifest__.py
+++ __manifest__.py
@@ -20,12 +20,5 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['crm'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ]
+    'depends': ['crm']
 }
