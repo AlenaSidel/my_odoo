eee8265cd1274ab0c6124edb21c02607b903d07b
Alena Sidel <sidelnikova.alean@gmail.com>
2017-06-29 10:37:48 +0500

    Переопределение email_from

--- models/models.py
+++ models/models.py
@@ -6,5 +6,4 @@ from odoo import models, fields
 class LeadLogg(models.Model):
     _inherit = "crm.lead"
 
-    email_from = fields.Char('Email', help="Email address of the contact",
-                             index=True, track_visibility='onchange')
+    email_from = fields.Char(track_visibility='onchange')
